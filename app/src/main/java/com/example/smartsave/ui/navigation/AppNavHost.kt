fd389f36eaed04378b04da57c7d6954d4f8277a7
vic <vikibedrova123@gmail.com>
2025-05-17 15:40:57 +0300

    fix landing

--- app/src/main/java/com/example/smartsave/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/smartsave/ui/navigation/AppNavHost.kt
@@ -5,6 +5,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.smartsave.ui.activity.AnalyticsScreen
+import com.example.smartsave.ui.activity.LandingScreen
 import com.example.smartsave.ui.activity.WithdrawScreen
 import com.example.smartsave.ui.activity.dashboard.DashboardScreen
 import com.example.smartsave.ui.activity.login.LoginScreen
@@ -12,7 +13,7 @@ import com.example.smartsave.ui.activity.smartSaveSetup.SmartSaveSetupScreen
 import com.example.smartsave.ui.activity.welcome.WelcomeScreen
 
 @Composable
-fun AppNavHost(startDestination: String = Screen.Welcome.route) {
+fun AppNavHost(startDestination: String = Screen.Landing.route) {
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = startDestination) {
@@ -22,5 +23,6 @@ fun AppNavHost(startDestination: String = Screen.Welcome.route) {
         composable(Screen.Setup.route) { SmartSaveSetupScreen(navController) }
         composable(Screen.Analytics.route){ AnalyticsScreen(navController) }
         composable(Screen.Withdraw.route){ WithdrawScreen(navController) }
+        composable(Screen.Landing.route){ LandingScreen(navController) }
     }
 }
